cc07a433d7ea47a2cd34a9cf396005b49b3297a0
Federico Andres Arnaudo <federicoarnaudo.97@gmail.com>
2025-06-05 11:27:08 -0300

    tests para division

--- Calculadora/src/test/java/CalculadoraTest.java
+++ Calculadora/src/test/java/CalculadoraTest.java
@@ -20,4 +20,51 @@ public class CalculadoraTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void testDividir_and_returnExactIntValue(){
+        // Arrange
+        int a = 10;
+        int b = 2;
+        int expected = 5;
+
+        // Act
+        int actual = calculadora.dividir(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDividir_and_returnAroundIntValue(){
+        // Arrange
+        int a = 4;
+        int b = 5;
+        int expected = 0;
+
+        // Act
+        int actual = calculadora.dividir(a, b);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testDividirPorCero_and_returnZero(){
+        // Arrange
+        int a = 4;
+        int b = 0;
+        String expected = "No se puede dividir por cero";
+
+        // Act
+        int actual = calculadora.dividir(a, b);
+
+        // Assert
+        IllegalArgumentException ex = assertThrows(
+                IllegalArgumentException.class,
+                () -> calculadora.dividir(4, 0)
+        );
+
+        assertEquals(expected, ex.getMessage());
+    }
+
 }
